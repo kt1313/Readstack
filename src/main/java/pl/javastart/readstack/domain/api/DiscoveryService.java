c636cd82259ee8f3796918bd4c8eaa4f5119e1da
kt1313 <tomaklim@wp.pl>
2021-06-07 03:29:34 +0200

    day4 - porządkowanie projektu, dodanie plikow jspf, jsp

--- src/main/java/pl/javastart/readstack/domain/api/DiscoveryService.java
+++ src/main/java/pl/javastart/readstack/domain/api/DiscoveryService.java
@@ -7,13 +7,28 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DiscoveryService {
+    private final DiscoveryDao discoveryDao = new DiscoveryDao();
 
-    //reszta bez zmian
+    public List<DiscoveryBasicInfo> findAll() {
+        return discoveryDao.findAll()
+                .stream().map(DiscoveryMapper::map)
+                .collect(Collectors.toList());
+    }
 
-    public List<DiscoveryBasicInfo> findByCategory(int categoryId) {
+    public List<DiscoveryBasicInfo> findAllByCategory(int categoryId) {
         return discoveryDao.findByCategory(categoryId)
                 .stream().map(DiscoveryMapper::map)
                 .collect(Collectors.toList());
     }
 
+    private static class DiscoveryMapper {
+        static DiscoveryBasicInfo map(Discovery d) {
+            return new DiscoveryBasicInfo(
+                    d.getTitle(),
+                    d.getUrl(),
+                    d.getDescription(),
+                    d.getDateAdded()
+            );
+        }
+    }
 }
